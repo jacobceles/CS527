75a2fcfc34e49d6cc80dd0253b3d9367b3280923
Jacob Celestine <jacobceles@gmail.com>
2021-02-13 06:16:54 +0530

    Adding code

---
+++ application.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template
+from db import ConnectMysql, ConnectRedshift
+
+app = Flask(__name__)
+
+
+@app.route('/querying', methods=["GET"])
+def query_db():
+    source = request.args.get('source', 'mysql')
+    query = request.args.get('query', 'show tables;')
+
+    if source.lower() == "mysql":
+        connection = ConnectMysql(host='instacart.cze09fdga760.us-east-2.rds.amazonaws.com', user='datastars',
+                                  password='CS527#Datastars', db='instacart')
+    else:
+        connection = ConnectRedshift(host=' redshift-cs527-group2.cebainumhmtq.us-east-1.redshift.amazonaws.com', 
+                                     user='datastars', password='CS527#Datastars', database='instacart', port=5439)
+
+    col_name, content, query_time = connection.run_query(query)
+    connection.disconnect()
+    if col_name is not None:
+        return render_template("index.html", headings=col_name, data=content,
+                               time_taken="<b>Time Elapsed: " + query_time + "</b>")
+    else:
+        return render_template("index.html", headings=None, data=None, time_taken=query_time)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ db.py
@@ -0,0 +1,58 @@
+import pymysql
+import psycopg2
+from time import time
+
+
+class ConnectMysql:
+    def __init__(self, host="localhost", user="root", password=None, db="instacart"):
+        self.db = pymysql.connect(host=host, user=user, password=password, db=db)
+        self.cursor = self.db.cursor()
+
+    def run_query(self, query_statement):
+        try:
+            start_time = int(round(time() * 1000))
+            self.cursor.execute(query_statement)
+            col_info = self.cursor.description
+            result = self.cursor.fetchall()
+            query_time = str(int(round(time() * 1000)) - start_time) + " ms"
+            if len(result) > 1000:
+                result = result[:99]
+                query_time += '\nThe result is too larger to transmit and display, so we limit the size to return'
+            col_name = []
+            for i in range(len(col_info)):
+                col_name.append(col_info[i][0])
+            return col_name, result, query_time
+        except Exception as err:
+            e = str(err)[1:-1].split(',')
+            return None, None, "Query failed with error code: " + str(e[0]) + "<br>Details: " + str(e[1][2:-1])
+
+    def disconnect(self):
+        self.db.close()
+
+
+class ConnectRedshift:
+    def __init__(self, host="localhost", database='instacart', user='root', password=None, port=5000):
+        self.con = psycopg2.connect(host=host, dbname=database, port=port, password=password, user=user)
+        self.cur = self.con.cursor()
+
+    def run_query(self, query_statement):
+        try:
+            start_time = int(round(time() * 1000))
+            self.cur.execute(query_statement)
+            col_info = self.cur.description
+            result = self.cur.fetchall()
+            query_time = str(int(round(time() * 1000)) - start_time) + " ms"
+            if len(result) > 1000:
+                result = result[:99]
+                query_time += '\nThe result is too larger to transmit and display, so we limit the size to return'
+            col_name = []
+            for i in range(len(col_info)):
+                col_name.append(col_info[i][0])
+            return col_name, result, query_time
+        except Exception as e:
+            print(e)
+            return e
+
+    def disconnect(self):
+        self.cur.close()
+        self.con.close()
