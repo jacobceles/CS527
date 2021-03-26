f166749f81000164dc660f0314af4a9cc13d089b
Jacob Celestine <jacobceles@gmail.com>
2021-03-26 22:04:31 +0530

    Updated to handle DDL statements
    Resolving issues #2 and #3

--- Project 1/db.py
+++ Project 1/db.py
@@ -12,21 +12,30 @@ class ConnectMysql:
         try:
             start_time = int(round(time() * 1000))
             self.cursor.execute(query_statement)
-            col_info = self.cursor.description
-            result = self.cursor.fetchall()
             query_time = str(int(round(time() * 1000)) - start_time) + " ms"
+
+            col_info = self.cursor.description
+            if col_info is None:
+                query_time = "<b>Time Elapsed: </b>" + query_time + \
+                             "<br><b>QUERY: </b>" + query_statement + \
+                             "<br>DDL statement successfully executed!"
+                return None, None, query_time
+
+            result = self.cursor.fetchall()
             if len(result) > 1000:
                 result = result[:99]
-                query_time += '<br>The result is too larger to transmit and display, so we limit the size to return'
+                query_time += '<br>The result is too larger to transmit and display, so we limit the size to return<br>'
             col_name = []
             for i in range(len(col_info)):
                 col_name.append(col_info[i][0])
             return col_name, result, query_time
         except Exception as err:
             e = str(err)[1:-1].split(',')
-            return None, None, "Query failed with error code: " + str(e[0]) + "<br>Details: " + str(e[1][2:-1])
+            return None, None, "<b>Query failed with error code: </b>" + str(e[0]) + "<br>Details: " + str(e[1][2:-1])
 
     def disconnect(self):
+        self.db.commit()
+        self.db.commit()
         self.db.close()
 
 
@@ -39,19 +48,27 @@ class ConnectRedshift:
         try:
             start_time = int(round(time() * 1000))
             self.cur.execute(query_statement)
-            col_info = self.cur.description
-            result = self.cur.fetchall()
             query_time = str(int(round(time() * 1000)) - start_time) + " ms"
+
+            col_info = self.cur.description
+            if col_info is None:
+                query_time = "<b>Time Elapsed: </b>" + query_time + \
+                             "<br><b>QUERY: </b>" + query_statement + \
+                             "<br>DDL statement successfully executed!"
+                return None, None, query_time
+
+            result = self.cur.fetchall()
             if len(result) > 1000:
                 result = result[:99]
-                query_time += '<br>The result is too larger to transmit and display, so we limit the size to return'
+                query_time += '<br>The result is too larger to transmit and display, so we limit the size to return<br>'
             col_name = []
             for i in range(len(col_info)):
                 col_name.append(col_info[i][0])
             return col_name, result, query_time
         except Exception as e:
-            return None, None, str(e).capitalize()
+            return None, None, "<b>Query failed with error: </b>" + str(e).capitalize()
 
     def disconnect(self):
+        self.con.commit()
         self.cur.close()
         self.con.close()
