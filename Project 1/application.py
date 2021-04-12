211ca49ecb94036a3010ea8a346f74be28216880
Jacob Celestine <jacobceles@gmail.com>
2021-04-12 13:06:40 +0530

    Updated to include mongo db

--- Project 1/application.py
+++ Project 1/application.py
@@ -1,7 +1,6 @@
-import io
 import pandas as pd
-from flask import Flask, request, render_template, jsonify, send_file, make_response
-from db import ConnectMysql, ConnectRedshift
+from flask import Flask, request, render_template, jsonify, make_response
+from db import ConnectMysql, ConnectRedshift, ConnectMongoDB
 from sql_keywords import generic_sql_keywords
 
 global df
@@ -16,7 +15,7 @@ def suggest():
     token = token.lower()
     source = source.lower()
 
-    if source == 'mysql' or source == 'redshift':
+    if source == 'mysql' or source == 'redshift' or source == 'mongodb':
         suggestions = filter(lambda x: x.lower().startswith(token), generic_sql_keywords)
         suggestions = [{'key': value} for value in suggestions]
     else:
@@ -36,9 +35,11 @@ def query_db():
     if source.lower() == "mysql":
         connection = ConnectMysql(host='instacart.cze09fdga760.us-east-2.rds.amazonaws.com', user='datastars',
                                   password='CS527#Datastars', db='instacart')
-    else:
+    elif source.lower() == "redshift":
         connection = ConnectRedshift(host='redshift-cs527-group2.cebainumhmtq.us-east-1.redshift.amazonaws.com',
                                      user='datastars', password='CS527#Datastars', database='instacart', port=5439)
+    else:
+        connection = ConnectMongoDB(server="127.0.0.1", database='instacart', port=27017)
 
     col_name, content, query_time, status = connection.run_query(query)
     connection.disconnect()
@@ -67,7 +68,6 @@ def download(filetype):
         resp.headers["Content-Type"] = "text/json"
         return resp
     elif filetype == 'html_file':
-        print("HTML")
         resp = make_response(df.to_html(header=True, index=False))
         resp.headers["Content-Disposition"] = "attachment; filename=results.html"
         resp.headers["Content-Type"] = "text/html"
--- Project 1/db.py
+++ Project 1/db.py
@@ -1,3 +1,4 @@
+import pyodbc
 import pymysql
 import psycopg2
 from time import time
@@ -44,22 +45,22 @@ class ConnectMysql:
 class ConnectRedshift:
     def __init__(self, host="localhost", database='instacart', user='root', password=None, port=5000):
         self.con = psycopg2.connect(host=host, dbname=database, port=port, password=password, user=user)
-        self.cur = self.con.cursor()
+        self.cursor = self.con.cursor()
 
     def run_query(self, query_statement):
         try:
             start_time = int(round(time() * 1000))
-            self.cur.execute(query_statement)
+            self.cursor.execute(query_statement)
             query_time = str(int(round(time() * 1000)) - start_time) + " ms"
 
-            col_info = self.cur.description
+            col_info = self.cursor.description
             if col_info is None:
                 query_time = "<b><i>DDL statement successfully executed!</i></b><br>"\
                              "<b>QUERY EXECUTED: </b><i>" + query_statement + "</i><br>" \
                              "<b>TIME ELAPSED: </b><i>" + query_time + "</i>"
                 return None, None, query_time, "success_ddl"
 
-            result = self.cur.fetchall()
+            result = self.cursor.fetchall()
             if len(result) > 1000:
                 result = result[:99]
                 query_time += '<br>The result is too large to transmit and display, so we limit the size to return.<br>'
@@ -72,5 +73,46 @@ class ConnectRedshift:
 
     def disconnect(self):
         self.con.commit()
-        self.cur.close()
+        self.cursor.close()
+        self.con.close()
+
+
+class ConnectMongoDB:
+    def __init__(self, server="127.0.0.1", database='instacart', port=27017):
+        self.con = pyodbc.connect('DRIVER={Devart ODBC Driver for MongoDB};'
+                                  'Server=' + server + ';Port=' + str(port) +
+                                  ';Database=' + database)
+        self.cursor = self.con.cursor()
+
+    def run_query(self, query_statement):
+        try:
+            start_time = int(round(time() * 1000))
+            self.cursor.execute(query_statement)
+            query_time = str(int(round(time() * 1000)) - start_time) + " ms"
+
+            col_info = self.cursor.description
+            if col_info is None:
+                query_time = "<b><i>DDL statement successfully executed!</i></b><br>"\
+                             "<b>QUERY EXECUTED: </b><i>" + query_statement + "</i><br>" \
+                             "<b>TIME ELAPSED: </b><i>" + query_time + "</i>"
+                return None, None, query_time, "success_ddl"
+
+            result = self.cursor.fetchall()
+            if len(result) > 1000:
+                result = result[:99]
+                query_time += '<br>The result is too large to transmit and display, so we limit the size to return.<br>'
+            results = [tuple(rows) for rows in result]
+            col_name = []
+            for i in range(len(col_info)):
+                col_name.append(col_info[i][0])
+            return col_name, results, query_time, "success_table"
+        except Exception as err:
+            e = str(err)[1:-1].split(',')
+            error = "<b>QUERY FAILED WITH ERROR CODE: </b><i>" + str(e[0]) + \
+                    "</i><br><b>DETAILS: </b><i>" + str(e[1][2:-1]) + "</i>"
+            return None, None, error, "failed"
+
+    def disconnect(self):
+        self.con.commit()
+        self.cursor.close()
         self.con.close()
